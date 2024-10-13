1728c8bb0ed8bb419ddd5701df3b4ceecb0c456e
mutshiv <vmutshinya@gmail.com>
2024-10-13 17:31:03 +0200

    Update telescope.lua

--- lua/mutshiv/lazy/telescope.lua
+++ lua/mutshiv/lazy/telescope.lua
@@ -7,10 +7,22 @@ return {
   	"nvim-lua/plenary.nvim"
   },
 	config = function () 
-		vim.keymap.set("n", "<leader>pg", function()
-		  local builtin = require('telescope.builtin')
-		  builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		  vim.keymap.set('n', "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		end)
-	end,
+	        require('telescope').setup({})
+	
+	        local builtin = require('telescope.builtin')
+	        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+	        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+	        vim.keymap.set('n', '<leader>pws', function()
+	            local word = vim.fn.expand("<cword>")
+	            builtin.grep_string({ search = word })
+	        end)
+	        vim.keymap.set('n', '<leader>pWs', function()
+	            local word = vim.fn.expand("<cWORD>")
+	            builtin.grep_string({ search = word })
+	        end)
+	        vim.keymap.set('n', '<leader>ps', function()
+	            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+	        end)
+	        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+	    end
 }
