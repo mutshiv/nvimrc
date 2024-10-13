7c338dfdd84fb6c768f0289b077f23ea927a2e0b
mutshiv <vmutshinya@gmail.com>
2024-10-13 22:52:32 +0200

    now tired of viming

--- lua/mutshiv/lazy/todo-comments.lua
+++ lua/mutshiv/lazy/todo-comments.lua
@@ -1,13 +1,19 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "TodoQuickFix",
+    cmd ={
+        "TodoQuickFix",
+        "TodoLocList",
+        "TodoTelescope",
+    },
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     },
     keys = {
-        vim.keymap.set('n', "<leader>f", "<cmd>TodoQuickFix<CR>", { desc = "TODO List"})
+        vim.keymap.set('n', "<leader>jq", "<cmd>TodoQuickFix<CR>", { desc = "TODO List"}),
+        vim.keymap.set('n', "<leader>jl", "<cmd>TodoLocList<CR>", { desc = "TODO Location"}),
+        vim.keymap.set('n', "<leader>jt", "<cmd>TodoTelescope<CR>", { desc = "TODO Telescope"}),
     }
 }
