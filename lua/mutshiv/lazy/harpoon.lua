fc7062e6122eaf76746c976c912b1bac60360e35
mutshiv <vmutshinya@gmail.com>
2024-10-13 20:04:59 +0200

    remaps

--- lua/mutshiv/lazy/harpoon.lua
+++ lua/mutshiv/lazy/harpoon.lua
@@ -8,13 +8,13 @@ return {
 
             harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, {desc = "H Mark File 1"})
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, {desc = "H Mark File 2"})
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, {desc = "H Mark File 3"})
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, {desc = "H Mark File 4"})
+            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, {desc = "H Mark File 5"})
         end
 }
--- lua/mutshiv/remap.lua
+++ lua/mutshiv/remap.lua
@@ -1,32 +1,35 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Current Browsong dir" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>", { desc = "Move Line 1 Down" })
+vim.keymap.set("v", "J", ":m '>+1<CR>", { desc = "Move Line 1 Up" })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", ":m '.+1<CR>", { desc = "Move Line 1 Up" })
+vim.keymap.set("n", "<C-k>", ":m '.-2<CR>", { desc = "Move Line 1 Down" })
+
+-- vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll Down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll Up" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- save current file
-vim.keymap.set("n", "<C-s>", "<cmd>:w<CR>")
-vim.keymap.set("n", "<C-q>", "<cmd>:q<CR>")
+vim.keymap.set("n", "<C-s>", "<cmd>:w<CR>", { desc = "Move Line 1 Down" })
+vim.keymap.set("n", "<C-q>", "<cmd>:q<CR>", { desc = "Move Line 1 Down" })
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+--vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+--vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
