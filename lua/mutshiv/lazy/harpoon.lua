f1a5d155011f679dbe248ee31dc12db765e27f2c
mutshiv <vmutshinya@gmail.com>
2024-10-13 22:28:08 +0200

    keystrokes

--- lua/mutshiv/lazy/harpoon.lua
+++ lua/mutshiv/lazy/harpoon.lua
@@ -8,13 +8,13 @@ return {
 
             harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, {desc = "Harpoon Mark-f"})
-            vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Harpoon Marked Files"})
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, {desc = "Harpoon Add"})
+            vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Harpoon List"})
 
-            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, {desc = "H Mark File 1"})
-            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, {desc = "H Mark File 2"})
-            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, {desc = "H Mark File 3"})
-            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, {desc = "H Mark File 4"})
-            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, {desc = "H Mark File 5"})
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, {desc = "Harpoon File 1"})
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, {desc = "Harpoon File 2"})
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, {desc = "Harpoon File 3"})
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, {desc = "Harpoon File 4"})
+            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, {desc = "Harpoon File 5"})
         end
 }
--- lua/mutshiv/lazy/todo-comments.lua
+++ lua/mutshiv/lazy/todo-comments.lua
@@ -1,9 +1,13 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "TodoQuickFix",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    keys = {
+        vim.keymap.set('n', "<leader>f", "<cmd>TodoQuickFix<CR>", { desc = "TODO List"})
+    }
 }
