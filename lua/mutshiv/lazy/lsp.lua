fdde9338698e9b7b2b3e5e5d18f9098ffaa158d7
mutshiv <vmutshinya@gmail.com>
2024-10-13 18:50:06 +0200

    yes

--- lua/mutshiv/lazy/lsp.lua
+++ lua/mutshiv/lazy/lsp.lua
@@ -29,6 +29,7 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "jdtls",
+                "ts_ls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
--- lua/mutshiv/remap.lua
+++ lua/mutshiv/remap.lua
@@ -12,7 +12,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
@@ -26,9 +25,6 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
