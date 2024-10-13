721d0e9947b675c7f55b655e2491c57db5505490
mutshiv <vmutshinya@gmail.com>
2024-10-13 18:45:54 +0200

    just testing

--- lua/mutshiv/lazy/telescope.lua
+++ lua/mutshiv/lazy/telescope.lua
@@ -6,9 +6,9 @@ return {
   dependencies = {
   	"nvim-lua/plenary.nvim"
   },
-	config = function () 
+	config = function ()
 	        require('telescope').setup({})
-	
+
 	        local builtin = require('telescope.builtin')
 	        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 	        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
