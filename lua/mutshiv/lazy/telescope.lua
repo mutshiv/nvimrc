d94d431f37ef679e7966a40d6646add430273813
mutshiv <vmutshinya@gmail.com>
2024-10-14 19:18:28 +0200

    Update telescope.lua

--- lua/mutshiv/lazy/telescope.lua
+++ lua/mutshiv/lazy/telescope.lua
@@ -4,13 +4,37 @@ return {
     tag = "0.1.8",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        { -- If encountering errors, see telescope-fzf-native README for installation instructions
+            'nvim-telescope/telescope-fzf-native.nvim',
+    
+            -- `build` is used to run some command when the plugin is installed/updated.
+            -- This is only run then, not every time Neovim starts up.
+            build = 'make',
+    
+            -- `cond` is a condition used to determine whether this plugin should be
+            -- installed and loaded.
+            cond = function()
+              return vim.fn.executable 'make' == 1
+            end,
+          },
+         { 'nvim-telescope/telescope-ui-select.nvim' },
     },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup {
+            extensions = {
+              ['ui-select'] = {
+                require('telescope.themes').get_dropdown(),
+              },
+            },
+        }
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      -- Enable Telescope extensions if they are installed
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+        
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
@@ -21,6 +45,7 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -28,7 +53,9 @@ return {
           previewer = false,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
-        
+
+      -- It's also possible to pass additional configuration options.
+      --  See `:help telescope.builtin.live_grep()` for information about particular keys
       vim.keymap.set('n', '<leader>s/', function()
         builtin.live_grep {
           grep_open_files = true,
@@ -40,6 +67,7 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+    end,
 
         -- Git information
         vim.keymap.set('n', '<leader>g', "<nop>", {desc = "Git"})
