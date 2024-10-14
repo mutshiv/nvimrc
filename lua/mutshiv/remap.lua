940e4554da0b95247e092202f4336449d99bf712
mutshiv <vmutshinya@gmail.com>
2024-10-14 19:07:14 +0200

    continuing

--- lua/mutshiv/remap.lua
+++ lua/mutshiv/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Current Browsing dir" })
 
+-- Java keymaps
+vim.keymap.set('n', '<leader>c', "<cmd>lua require('jdtls').organize_imports()<cr>", {desc = 'Code'})
+
 local opts = { noremap = true, silent = true }
 
 -- Normal mode mapping
