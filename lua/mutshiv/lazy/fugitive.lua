656c31a89be28e22d75c4c30afc1904c67c54bf6
vusanim <vusani.mutshinya@sita.co.za>
2025-01-09 19:59:32 +0200

    feat: more fugitive

--- lua/mutshiv/lazy/fugitive.lua
+++ lua/mutshiv/lazy/fugitive.lua
@@ -1,3 +1,11 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+
+    config = function()
+        vim.keymap.set("n", "<leader>gp", function ()
+            vim.cmd.Git('push')
+        end, {desc = 'Git Push'})
+
+        vim.keymap.set("n", "<leader>ga", "<cmd>Gwrite <CR>", {desc = 'Git Add'})
+    end,
 }
