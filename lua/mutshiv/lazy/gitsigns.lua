2448dd7ed8d2df69fe4bc5a23400ad1d56de14e2
vusanim <vusani.mutshinya@sita.co.za>
2025-01-09 19:32:00 +0200

    feat: fugitive on the move

--- lua/mutshiv/lazy/gitsigns.lua
+++ lua/mutshiv/lazy/gitsigns.lua
@@ -41,10 +41,10 @@ return {
                     map('n', '<leader>hR', gs.reset_buffer)
                     map('n', '<leader>hp', gs.preview_hunk)
                     map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-                    map('n', '<leader>tb', gs.toggle_current_line_blame)
-                    map('n', '<leader>hd', gs.diffthis)
+                    map('n', '<leader>tb', gs.toggle_current_line_blame, {desc = 'Git Line Blame'})
+                    map('n', '<leader>hd', gs.diffthis, {desc = 'Git Diff'})
                     map('n', '<leader>hD', function() gs.diffthis('~') end)
-                    map('n', '<leader>td', gs.toggle_deleted)
+                    map('n', '<leader>td', gs.toggle_deleted, {desc = 'Git Show Deleted'})
 
                     -- Text object
                     map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
