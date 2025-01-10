829bc1fe5a70ba80ced1cca311b807b2350bf976
mutshiv <vmutshinya@gmail.com>
2025-01-10 09:41:13 +0200

    feat: pull implemented

--- lua/mutshiv/lazy/fugitive.lua
+++ lua/mutshiv/lazy/fugitive.lua
@@ -2,11 +2,12 @@ return {
     "tpope/vim-fugitive",
 
     config = function()
-        vim.keymap.set("n", "<leader>gp", function ()
+        vim.keymap.set("n", "<leader>gp", function()
             vim.cmd.Git('push')
-        end, {desc = 'Git Push'})
+        end, { desc = 'Git Push' })
 
-        vim.keymap.set("n", "<leader>ga", "<cmd>G <CR>", {desc = 'Git Add'})
-        vim.keymap.set("n", "<leader>gcc", "<cmd>Git commit <CR>", {desc = 'Git Add'})
+        vim.keymap.set("n", "<leader>ga", "<cmd>G <CR>", { desc = 'Git Add' })
+        vim.keymap.set("n", "<leader>gcc", "<cmd>Git commit <CR>", { desc = 'Git Commit' })
+        vim.keymap.set("n", "<leader>gP", "<cmd>Git pull <CR>", { desc = 'Git Pull' })
     end,
 }
